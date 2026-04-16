76eac09966136d530eb372af2cc409c36e6b7b91
gruvw <gruvw@users.noreply.github.com>
2026-04-16 13:40:37 +0000

    deploy: ae003fac0edfffaffc7f37dd23904e879263ecc2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-import { BubbleMap } from "./bubble_map.js";
-import { teams, teamsPositions } from "./data.js";
+import { BubbleMap } from "./BubbleMap.js";
+import { SeasonsLoader } from "./SeasonsLoader.js";
+import * as Stats from "./stats.js";
 
+// Team Bubble Map
 new BubbleMap({
   containerSelector: "#teams",
-  items: teams,
-  positions: teamsPositions,
+  seasonsLoader: new SeasonsLoader("./data/team_seasons.csv", (row) => row.teamName),
+  statsUpdate: Stats.updateTeamStats,
+  attributes: [
+    // display name, row to value function, aggregation function
+    ["Wins", (r) => parseFloat(r["win"])],
+    ["Average points", (r) => parseFloat(r["teamScore"])],
+    ["Three points %", (r) => parseFloat(r["threePointersPercentage"])],
+    ["Assists", (r) => parseFloat(r["assists"])],
+    ["Rebounds", (r) => parseFloat(r["rebounds"])],
+    ["Blocks", (r) => parseFloat(r["blocks"])],
+    ["Steals", (r) => parseFloat(r["steals"])],
+    // ["Win %", (r) => parseFloat(r["win"]) / parseFloat(r["gamesPlayed"])],
+  ],
 });
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,12 @@
 export function clamp(value, min, max) {
   return Math.min(max, Math.max(min, value));
 }
+
+export function getRandomThree(arr) {
+  const shuffled = [...arr];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap
+  }
+  return shuffled.slice(0, 3);
+}
